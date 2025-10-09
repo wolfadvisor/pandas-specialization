2027f345d040fd2e5e6220b53afb151fe07da99a
wolfadvisor <carlos@wolfbusinessadvisor.com>
2025-10-09 09:29:27 -0300

    semana 03 visualização de dados exportação png-matplotlib e seaborn

--- ClassesApoio/DataManager.py
+++ ClassesApoio/DataManager.py
@@ -1,23 +1,55 @@
 import os
+import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
 
 
 class DataManager:
     @staticmethod
-    def save_files_clean(df, filename, path):
-
+    def save_files_clean(df, filename=None, path=None):
         if path is None:
-            path =r'C:\\Users\\User\\PycharmProjects\\PythonProject\\data\\limpos'
+            path = r'C:\Users\User\PycharmProjects\PythonProject\data\limpos'
 
-        os.makedirs(path,exist_ok=True)
+        os.makedirs(path, exist_ok=True)
 
         hoje = datetime.date.today().strftime('%Y%m%d')
-        if filename is None:
-            filename = input(f'Digite o nome do arquivo sem extensão: {hoje}_')
+        if not filename:
+            filename = input(f'Digite o nome do arquivo sem extensão ({hoje}_): ')
 
-        file_path = os.path.join(path,f'{filename}_{hoje}.csv')
+        file_path = os.path.join(path, f'{filename}_{hoje}.csv')
 
-        df.to_csv(file_path,index=False,encoding='utf-8-sig')
-        print(f'Arquivo salvo com sucesso: \n{file_path}\n{path}')
+        df.to_csv(file_path, index=False, encoding='utf-8-sig')
+        print(f'✅ Arquivo salvo com sucesso:\n{file_path}')
         return file_path
+
+    @staticmethod
+    def save_plot(fig=None, filename=None, path=None, show=False, format='png'):
+        if path is None:
+            path = r'C:\Users\User\PycharmProjects\PythonProject\reports'
+
+        os.makedirs(path, exist_ok=True)
+        hoje = datetime.date.today().strftime('%Y%m%d')
+
+        if not filename:
+            filename = input(f'Digite o nome do gráfico sem extensão ({hoje}_): ')
+
+        file_path = os.path.join(path, f'{filename}_{hoje}.{format}')
+
+        try:
+            # Se nenhuma figura for passada, usa a atual
+            if fig is None:
+                fig = plt.gcf()
+
+            # Salva a figura (sem limpar o buffer)
+            fig.savefig(file_path, bbox_inches='tight', dpi=300)
+
+            if show:
+                plt.show()
+
+            print(f'✅ Gráfico salvo com sucesso:\n{file_path}')
+            return file_path
+
+        except Exception as e:
+            print(f'❌ Erro ao salvar o gráfico: {e}')
+            return None
+
---
+++ scripts/Semana03/semana03_dia08.py
@@ -0,0 +1,66 @@
+# Dia 08 – Visualização de Dados com Matplotlib e Seaborn
+# Objetivo: Criar gráficos de linha para evolução mensal das commodities
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+from matplotlib.pyplot import title
+from pandas.conftest import index
+
+from ClassesApoio.DataManager import DataManager
+
+#Configurando estetica global do Seaborn
+sns.set_theme(style='whitegrid',palette='pastel',context="talk")
+
+#Carregando o DataSet Limpo.
+
+filepath = r'C:/Users/User/PycharmProjects/PythonProject/data/limpos/producao_mes_arrumado.csv'
+
+df= pd.read_csv(filepath)
+
+#Agrupar dados por Mês e e Commodity e somar Produção e Exportação
+#df['Exportacoes'] = df['Exportacoes'].map(lambda x:f'{x:.2f}MT')
+df_groupby_Month_Commodity = df.groupby(['Mes','Commodity'],as_index=False)[['Producao','Exportacoes']].sum().round(2)
+#print(df_groupby_Month_Commodity)
+
+#Fazer um Ranking Top 3Mais produzidas e mais exportadas
+top3Prod = df_groupby_Month_Commodity.groupby('Commodity')['Producao'].sum().nlargest(3).index
+df_top3Prod = df_groupby_Month_Commodity[df_groupby_Month_Commodity['Commodity'].isin(top3Prod)].copy()
+print(top3Prod)
+print(df_top3Prod)
+top3Export =df_groupby_Month_Commodity.groupby('Commodity')['Exportacoes'].sum().nlargest(3).index
+df_top3Export =df_groupby_Month_Commodity[df_groupby_Month_Commodity['Exportacoes'].isin(top3Export)].copy()
+#print(df.info())
+print(top3Export)
+print(df_top3Export)
+
+#Colocar os meses em ordem cronologica
+ordem_meses = ['Jan','Fev','Mar','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez']
+df_top3Prod['Mes'] = pd.Categorical(df_top3Prod['Mes'],categories=ordem_meses, ordered=True)
+df_top3Export['Mes'] = pd.Categorical(df_top3Export['Mes'], categories=ordem_meses,ordered=True)
+
+#Plotar a evolução da produção e exportação
+
+plt.figure(figsize=(10,6))
+sns.lineplot(data=df_top3Prod,x='Mes',y='Producao',hue='Commodity',markers=0)
+plt.title('Evolução Mensal da Produção de Commodity',fontsize=14)
+plt.xlabel('Mês')
+plt.ylabel('Produção (Toneladas)')
+plt.legend(title='Commodity')
+plt.tight_layout()
+DataManager.save_plot(filename='evolucao-mensal-producao-commodities',show=True)
+plt.show()
+
+plt.figure(figsize=(10,6))
+sns.lineplot(data=df_top3Prod,x='Mes',y='Exportacoes',hue='Commodity',markers=0)
+plt.title('Evolução Mensal da Exportações de Commodity',fontsize=14)
+plt.xlabel('Mês')
+plt.ylabel('Exportações (Toneladas)')
+plt.legend(title='Commodity')
+plt.tight_layout()
+
+
+DataManager.save_plot(filename='evolucao-mensal-exportacoes-commodities',show=True)
+plt.show()
